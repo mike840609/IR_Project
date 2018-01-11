41632a4959aefa3c166f29b971788b6796b9f9af
mike840609 <mike840609@gmail.com>
2018-01-11 20:41:15 +0800

    dump data

--- Model/Formatter.py
+++ Model/Formatter.py
@@ -1,6 +1,10 @@
 import sys
 import re
 import json
+import pickle
+
+from Model.Poter_Algo.PorterStemmer import *
+from nltk.stem import WordNetLemmatizer
 
 
 class Formatter:
@@ -9,6 +13,7 @@ class Formatter:
 
     #  { id : dict }  user dictionary 
     posts_dic = {}
+    term_dictionay = {}
 
     def __init__(self , json_path = None):
         with open(json_path, 'r') as f:
@@ -26,22 +31,44 @@ class Formatter:
         self.stopWord_list = stop_temp.lower().split()
 
     
+    stemmer = PorterStemmer()
+    wnl = WordNetLemmatizer()
+
     #  tokenization
     def tokenize(self):
 
         for key , post in  self.posts_dic.items() : 
 
-            text = post["text"].encode('utf-8').lower()
+            # text = post["text"].encode('utf-8').lower()
+            text = post["text"].lower()
+
             text = re.sub('[^a-zA-Z]+', ' ', text)
             text_arr = text.split()
             
             #  stopword operation 
             text_arr = [x for x in text_arr if x not in self.stopWord_list ]
-            
+
+            for idx , word in enumerate( text_arr ):
+                # stemming or lemmatization
+
+                # after_stm = self.stemmer.stem(word , 0 , len(word)-1)
+                after_stm = self.wnl.lemmatize(word)
+
+                text_arr[idx] = after_stm
+
             #  add to dict
             self.posts_dic[key]["stem_arr"] = text_arr
-            print (json.dumps(self.posts_dic[key] , indent=4))   
 
-            break
+    
+    def saveToPickle(self):
+        pickle.dump(self.posts_dic , open("posts_dict.p","wb"))
+    
+    def loadPickle(self):
+        self.posts_dic = pickle.load(open("posts_dict.p", 'rb'))
 
-            
+    def genDictionary (self):
+        pass
+
+    def getPosts_dict(self):
+        return self.posts_dic
+        
--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from  Model.Formatter import Formatter
+import json 
 import os
+import sys
+
+# pip freeze > requirements.txt
+
 
 def main():
-    formatter = Formatter('StaticDoc/trump_posts_test.json')
-    # formatter = Formatter('StaticJson/trump_posts.json')
-    formatter.tokenize()
+    
+    # formatter = Formatter('StaticDoc/trump_simple.json')
+    formatter = Formatter('StaticDoc/trump_simple_test.json')
+    # formatter.tokenize()
+    # formatter.saveToPickle()
+    formatter.loadPickle()
+    posts_dict = formatter.getPosts_dict()
+    
+
+    print (json.dumps(posts_dict, indent=2))
+    # print (json.dumps(formatter.getPosts_dict(), indent=2))
+
+
+
 
 if __name__ == '__main__':
     main()
