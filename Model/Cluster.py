12a8956f1347cdfa2f572bb3268d88e46e510ff3
DESKTOP-BNH19L5\ianchen <mike840609@gmail.com>
2018-01-15 21:01:55 +0800

    final

--- Model/Cluster.py
+++ Model/Cluster.py
@@ -18,7 +18,10 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 from sklearn.metrics import adjusted_rand_score
-# import pickle as pk
+
+import pickle as pk
+
+import sys
 
 class Cluster :
 
@@ -49,41 +52,40 @@ class Cluster :
 
         score = [] 
         #K = [10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100] 
-        K = list(range(2,61))
-        for k in K: 
-            KMeanModel_1 = KMeans(n_clusters=k, init='k-means++', max_iter=300, random_state= 5,tol=1e-1 , n_jobs=-1).fit(X)
-
-            KMeanModel_2 = KMeans(n_clusters=k, init='k-means++', max_iter=300 , random_state=7 , tol=1e-1, n_jobs=-1).fit(X)
-
-            rss = min(KMeanModel_1.inertia_ , KMeanModel_2.inertia_)
-            score.append(rss)
-
-            print(str(k) + " : " + str(rss))
-
-        print (score)
-        plt.plot(K, score ,'bx-')
-        plt.xlabel('k')
-        plt.ylabel('Distortion')
-        plt.title('The Elbow Method showing the optimal k')
-        plt.xticks(range(2,61,1))
-        plt.savefig('final.png')
-        plt.show()
-        
 
 
-        # feature terms 
-        '''
-        print("Top terms per cluster:")
-        order_centroids = model.cluster_centers_.argsort()[:, ::-1]
-        terms = vectorizer.get_feature_names()
-        
-        for i in range(2):
-            print("Cluster %d:" % i)
-            for ind in order_centroids[i, :10]:
-                print(' %s' % terms[ind]),
-            print("=================")
-        '''
+        model_13 = KMeans(n_clusters=13, init='k-means++', max_iter= sys.maxsize, random_state= 5 , n_jobs=-1).fit(X)
+        pk.dump(model_13 ,  open("model_13.p","wb"))
+        print(" 13 : " +   str(model_13.inertia_))
 
+        model_33 = KMeans(n_clusters=33, init='k-means++', max_iter= sys.maxsize, random_state= 5 , n_jobs=-1).fit(X)
+        pk.dump(model_33 ,  open("model_33.p","wb"))
+        print( " 33  : " +  str(model_33.inertia_))
+
+
+        # K = list(range(2,61))
+        # for k in K: 
+        #     KMeanModel_1 = KMeans(n_clusters=k, init='k-means++', max_iter=300, random_state= 5,tol=1 , n_jobs=-1).fit(X)
+
+        #     KMeanModel_2 = KMeans(n_clusters=k, init='k-means++', max_iter=300 , random_state=7 , tol=1, n_jobs=-1).fit(X)
+
+        #     rss = min(KMeanModel_1.inertia_ , KMeanModel_2.inertia_)
+        #     score.append(rss)
+
+        #     print(str(k) + " : " + str(rss))
+
+        # print (score)
+        # plt.plot(K, score ,'bx-')
+        # plt.xlabel('k')
+        # plt.ylabel('Distortion')
+        # plt.title('The Elbow Method showing the optimal k')
+        # plt.xticks(range(2,61,1))
+        # plt.savefig('final.png')
+        # plt.show()
+    
+
+
+        # feature terms ===================================================
 
         # print("\n")
         # print("Prediction")
